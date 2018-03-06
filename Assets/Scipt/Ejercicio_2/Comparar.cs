b970645f8992b4f7831f5b4c9ff927e0602bc27e
abraham-tl <abraham-tl>
2018-03-05 21:13:10 -0500

    Ejercicio 2
    Se inicio el ejercicio # 2 (DOC 22 - 23)

---
+++ Assets/Scipt/Ejercicio_2/Comparar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comparar : MonoBehaviour {
+    int aux;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Compara(GameObject[,] esferas,Color[] colores,int col,int fila)
+    {
+       
+        aux = 0;
+        for (int i =0; i <= fila;i++)
+        {
+            for (int j =0;j<= col;j++)
+            {
+                if (j> 0)
+                {
+                    if (esferas[i, j].GetComponent<Renderer>().material.color == esferas[i, j - 1].GetComponent<Renderer>().material.color)
+                    {
+                        print("HOLAAAAA");
+                        print(i);
+                        print(j & j-1);
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scipt/Ejercicio_2/Crear_esfera.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crear_esfera : MonoBehaviour {
+    public bool crear;
+    int filas;
+    int columnas;
+    Color[] colore;
+    int color;
+    GameObject[,] esferas;
+
+    public Comparar comparar;
+    public bool compa;
+	// Use this for initialization
+	void Start () {
+        colore = new Color[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (crear )
+        {
+            Colores();
+            Crear_matriz();
+            crear = false;
+            
+        }
+        else
+        {
+            if (compa )
+            {
+                comparar.Compara(esferas,colore,columnas ,filas);
+                compa = false;
+            }
+        }
+	}
+    void Crear_matriz()
+    {
+        columnas = Random.Range(3, 13);
+        filas = Random.Range(3, 13);
+        esferas= new GameObject [filas,columnas];
+
+        for (int i = 0; i<filas ; i++)
+        {
+            for (int j = 0;j<columnas;j++)
+            {
+                color = Random.Range(0,3);
+                GameObject esfera = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                esfera.transform.position  = new Vector3(i * 1.2f, j*1.2f, 0);
+                esfera.GetComponent<Renderer>().material.color = colore[color];
+                esferas[i,j] = esfera;
+            }
+           
+        }
+       
+    }
+    void Colores()
+    {
+        colore[0] = Color.blue;
+        colore[1] = Color.red;
+        colore[2] = Color.yellow;
+        colore[3] = Color.green ;
+    }
+
+}
